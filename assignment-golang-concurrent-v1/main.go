97fc822ca9587f6bb1be7e4717b16385567a9ed6
ghaidaazizah <114546678+ghaidaazizah@users.noreply.github.com>
2024-11-26 14:16:47 +0700

    Add files via upload

---
+++ assignment-golang-concurrent-v1/main.go
@@ -0,0 +1,329 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"a21hc3NpZ25tZW50/helper"
+	"a21hc3NpZ25tZW50/model"
+)
+
+type StudentManager interface {
+	Login(id string, name string) (string, error)
+	Register(id string, name string, studyProgram string) (string, error)
+	GetStudyProgram(code string) (string, error)
+	ModifyStudent(name string, fn model.StudentModifier) (string, error)
+}
+
+type InMemoryStudentManager struct {
+	sync.Mutex
+	students             []model.Student
+	studentStudyPrograms map[string]string
+	failedLoginAttempts  map[string]int
+}
+
+func NewInMemoryStudentManager() *InMemoryStudentManager {
+	return &InMemoryStudentManager{
+		students: []model.Student{
+			{
+				ID:           "A12345",
+				Name:         "Aditira",
+				StudyProgram: "TI",
+			},
+			{
+				ID:           "B21313",
+				Name:         "Dito",
+				StudyProgram: "TK",
+			},
+			{
+				ID:           "A34555",
+				Name:         "Afis",
+				StudyProgram: "MI",
+			},
+		},
+		studentStudyPrograms: map[string]string{
+			"TI": "Teknik Informatika",
+			"TK": "Teknik Komputer",
+			"SI": "Sistem Informasi",
+			"MI": "Manajemen Informasi",
+		},
+		failedLoginAttempts: make(map[string]int),
+	}
+}
+
+func ReadStudentsFromCSV(filename string) ([]model.Student, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 3 // ID, Name and StudyProgram
+
+	var students []model.Student
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			break
+		}
+		student := model.Student{
+			ID:           record[0],
+			Name:         record[1],
+			StudyProgram: record[2],
+		}
+		students = append(students, student)
+	}
+	return students, nil
+}
+
+func (sm *InMemoryStudentManager) Register(id string, name string, studyProgram string) (string, error) {
+	sm.Lock()
+	defer sm.Unlock()
+
+	// Check if student already exists
+	for _, student := range sm.students {
+		if student.ID == id {
+			return "", fmt.Errorf("student already registered")
+		}
+	}
+
+	// Add new student
+	sm.students = append(sm.students, model.Student{
+		ID:           id,
+		Name:         name,
+		StudyProgram: studyProgram,
+	})
+
+	return "Registration successful", nil
+}
+
+func (sm *InMemoryStudentManager) Login(id string, name string) (string, error) {
+	sm.Lock()
+	defer sm.Unlock()
+
+	// Check if ID is blocked
+	if sm.failedLoginAttempts[id] >= 3 {
+		return "", fmt.Errorf("Login gagal: Batas maksimum login terlampaui")
+	}
+
+	// Find student
+	for _, student := range sm.students {
+		if student.ID == id && student.Name == name {
+			sm.failedLoginAttempts[id] = 0 // Reset failed attempts
+			studyProgram := sm.studentStudyPrograms[student.StudyProgram]
+			return fmt.Sprintf("Selamat datang %s! Kamu terdaftar di program studi: %s.", student.Name, studyProgram), nil
+		}
+	}
+
+	// Increment failed login attempts
+	sm.failedLoginAttempts[id]++
+	return "", fmt.Errorf("Login gagal: data mahasiswa tidak ditemukan")
+}
+
+func (sm *InMemoryStudentManager) ImportStudents(filenames []string) error {
+	var wg sync.WaitGroup
+	for _, filename := range filenames {
+		wg.Add(1)
+		go func(file string) {
+			defer wg.Done()
+			students, err := ReadStudentsFromCSV(file)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			for _, student := range students {
+				sm.Register(student.ID, student.Name, student.StudyProgram)
+			}
+		}(filename)
+	}
+	wg.Wait()
+	return nil
+}
+
+func (sm *InMemoryStudentManager) SubmitAssignments(total int) {
+	tasks := make(chan int, total)
+	results := make(chan string, total)
+
+	for i := 1; i <= 3; i++ {
+		go func(workerID int) {
+			for task := range tasks {
+				time.Sleep(3 * time.Second) // Simulate long process
+				results <- fmt.Sprintf("Worker %d: Finished assignment %d", workerID, task)
+			}
+		}(i)
+	}
+
+	for i := 1; i <= total; i++ {
+		tasks <- i
+	}
+	close(tasks)
+
+	for i := 1; i <= total; i++ {
+		fmt.Println(<-results)
+	}
+}
+
+func main() {
+	manager := NewInMemoryStudentManager()
+
+	for {
+		helper.ClearScreen()
+		students := manager.GetStudents()
+		for _, student := range students {
+			fmt.Printf("ID: %s\n", student.ID)
+			fmt.Printf("Name: %s\n", student.Name)
+			fmt.Printf("Study Program: %s\n", student.StudyProgram)
+			fmt.Println()
+		}
+
+		fmt.Println("Selamat datang di Student Portal!")
+		fmt.Println("1. Login")
+		fmt.Println("2. Register")
+		fmt.Println("3. Get Study Program")
+		fmt.Println("4. Modify Student")
+		fmt.Println("5. Bulk Import Student")
+		fmt.Println("6. Submit assignment")
+		fmt.Println("7. Exit")
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Pilih menu: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		switch input {
+		case "1":
+			helper.ClearScreen()
+			fmt.Println("=== Login ===")
+			fmt.Print("ID: ")
+			id, _ := reader.ReadString('\n')
+			id = strings.TrimSpace(id)
+
+			fmt.Print("Name: ")
+			name, _ := reader.ReadString('\n')
+			name = strings.TrimSpace(name)
+
+			msg, err := manager.Login(id, name)
+			if err != nil {
+				fmt.Printf("Error: %s\n", err.Error())
+			}
+			fmt.Println(msg)
+			// Wait until the user presses any key
+			fmt.Println("Press any key to continue...")
+			reader.ReadString('\n')
+		case "2":
+			helper.ClearScreen()
+			fmt.Println("=== Register ===")
+			fmt.Print("ID: ")
+			id, _ := reader.ReadString('\n')
+			id = strings.TrimSpace(id)
+
+			fmt.Print("Name: ")
+			name, _ := reader.ReadString('\n')
+			name = strings.TrimSpace(name)
+
+			fmt.Print("Study Program Code (TI/TK/SI/MI): ")
+			code, _ := reader.ReadString('\n')
+			code = strings.TrimSpace(code)
+
+			msg, err := manager.Register(id, name, code)
+			if err != nil {
+				fmt.Printf("Error: %s\n", err.Error())
+			}
+			fmt.Println(msg)
+			// Wait until the user presses any key
+			fmt.Println("Press any key to continue...")
+			reader.ReadString('\n')
+		case "3":
+			helper.ClearScreen()
+			fmt.Println("=== Get Study Program ===")
+			fmt.Print("Program Code (TI/TK/SI/MI): ")
+			code, _ := reader.ReadString('\n')
+			code = strings.TrimSpace(code)
+
+			if studyProgram, err := manager.GetStudyProgram(code); err != nil {
+				fmt.Printf("Error: %s\n", err.Error())
+			} else {
+				fmt.Printf("Program Studi: %s\n", studyProgram)
+			}
+			// Wait until the user presses any key
+			fmt.Println("Press any key to continue...")
+			reader.ReadString('\n')
+		case "4":
+			helper.ClearScreen()
+			fmt.Println("=== Modify Student ===")
+			fmt.Print("Name: ")
+			name, _ := reader.ReadString('\n')
+			name = strings.TrimSpace(name)
+
+			fmt.Print("Program Studi Baru (TI/TK/SI/MI): ")
+			code, _ := reader.ReadString('\n')
+			code = strings.TrimSpace(code)
+
+			msg, err := manager.ModifyStudent(name, manager.ChangeStudyProgram(code))
+			if err != nil {
+				fmt.Printf("Error: %s\n", err.Error())
+			}
+			fmt.Println(msg)
+
+			// Wait until the user presses any key
+			fmt.Println("Press any key to continue...")
+			reader.ReadString('\n')
+		case "5":
+			helper.ClearScreen()
+			fmt.Println("=== Bulk Import Student ===")
+
+			// Define the list of CSV file names
+			csvFiles := []string{"students1.csv", "students2.csv", "students3.csv"}
+
+			err := manager.ImportStudents(csvFiles)
+			if err != nil {
+				fmt.Printf("Error: %s\n", err.Error())
+			} else {
+				fmt.Println("Import successful!")
+			}
+
+			// Wait until the user presses any key
+			fmt.Println("Press any key to continue...")
+			reader.ReadString('\n')
+
+		case "6":
+			helper.ClearScreen()
+			fmt.Println("=== Submit Assignment ===")
+
+			// Enter how many assignments you want to submit
+			fmt.Print("Enter the number of assignments you want to submit: ")
+			numAssignments, _ := reader.ReadString('\n')
+
+			// Convert the input to an integer
+			numAssignments = strings.TrimSpace(numAssignments)
+			numAssignmentsInt, err := strconv.Atoi(numAssignments)
+
+			if err != nil {
+				fmt.Println("Error: Please enter a valid number")
+			}
+
+			manager.SubmitAssignments(numAssignmentsInt)
+
+			// Wait until the user presses any key
+			fmt.Println("Press any key to continue...")
+			reader.ReadString('\n')
+		case "7":
+			helper.ClearScreen()
+			fmt.Println("Goodbye!")
+			return
+		default:
+			helper.ClearScreen()
+			fmt.Println("Pilihan tidak valid!")
+			helper.Delay(5)
+		}
+
+		fmt.Println()
+	}
+}
